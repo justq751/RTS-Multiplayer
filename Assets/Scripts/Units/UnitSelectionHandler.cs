f3528106c94d972faf982c2d8669e4f54335bd7d
VladD <dmitriev.vs.751@mail.ru>
2020-12-04 17:31:30 +0300

    Visual unit selection and deselection

---
+++ Assets/Scripts/Units/UnitSelectionHandler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using System;
+
+public class UnitSelectionHandler : MonoBehaviour
+{
+    private Camera mainCamera;
+    private List<Unit> selectedUnits = new List<Unit>();
+
+    [SerializeField] private LayerMask layerMask = new LayerMask();
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            foreach (Unit selectedUnit in selectedUnits)
+            {
+                selectedUnit.Deselect();
+            }
+            selectedUnits.Clear();
+        }
+        else if (Mouse.current.leftButton.wasReleasedThisFrame)
+        {
+            ClearSelectionArea();
+        }
+    }
+
+    private void ClearSelectionArea()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, layerMask)) { return; }
+        if(!hit.collider.TryGetComponent<Unit>(out Unit unit)) { return; }
+        if(!unit.hasAuthority) { return; }
+        selectedUnits.Add(unit);
+        foreach(Unit selecteUnit in selectedUnits)
+        {
+            selecteUnit.Select();
+        }
+    }
+}
