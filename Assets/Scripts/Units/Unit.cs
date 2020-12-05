33187c48d423a776cc740d5645772fa37fd2d220
VladD <dmitriev.vs.751@mail.ru>
2020-12-05 19:10:55 +0300

    Unit lists for each player

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using UnityEngine.Events;
+using System;
 
 public class Unit : NetworkBehaviour
 {
@@ -10,10 +11,30 @@ public class Unit : NetworkBehaviour
     [SerializeField] private UnityEvent onSelected = null;
     [SerializeField] private UnityEvent onDeselected = null;
 
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
     public UnitMovement GetUnitMovement()
     {
         return unitMovement;
     }
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
 
     #region Client
 
@@ -29,6 +50,18 @@ public class Unit : NetworkBehaviour
     {
         if (!hasAuthority) { return; }
         onDeselected?.Invoke();
+    }
+
+    public override void OnStartClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient()
+    {
+        if (!isClientOnly || !hasAuthority) { return; }
+        AuthorityOnUnitDespawned(this);
     }
 
     #endregion
