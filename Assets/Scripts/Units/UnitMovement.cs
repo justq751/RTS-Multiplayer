3750c69d09c5458d69541c12099dc6cd069a9cbb
VladD <dmitriev.vs.751@mail.ru>
2020-12-07 14:54:08 +0300

    Unit use stopping distance and reset path, no longer infinite pushing each other

--- Assets/Scripts/Units/UnitMovement.cs
+++ Assets/Scripts/Units/UnitMovement.cs
@@ -8,6 +8,14 @@ public class UnitMovement : NetworkBehaviour
 
     #region Server
 
+    [ServerCallback]
+    private void Update()
+    {
+        if(!agent.hasPath) { return; }
+        if(agent.remainingDistance > agent.stoppingDistance) { return; }
+        agent.ResetPath();
+    }
+
     [Command] public void CmdMove(Vector3 position)
     {
         if (!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)) { return; }
@@ -16,27 +24,8 @@ public class UnitMovement : NetworkBehaviour
 
     #endregion
 
-    /*
+    
     #region Client
 
-    public override void OnStartAuthority()
-    {
-        mainCamera = Camera.main;
-    }
-
-    [ClientCallback]
-    private void Update()
-    {
-        if (!hasAuthority) { return; }
-        
-        if (!Mouse.current.rightButton.wasPressedThisFrame) { return; }
-
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity)) { return; }
-
-        CmdMove(hit.point);
-    }
-
-    #endregion*/
+    #endregion
 }
